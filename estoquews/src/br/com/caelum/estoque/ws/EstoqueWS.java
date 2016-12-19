f07b2ff449f616c5f95e6e5159f3c5c121f56e87
Marcio Carvalho <mhn.carvalho@gmail.com>
2016-12-19 00:34:51 -0200

    Aula 2 - Personalizando o serviço

--- estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -2,17 +2,32 @@ package br.com.caelum.estoque.ws;
 
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
+import br.com.caelum.estoque.modelo.item.Filtro;
+import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ListaItens;
 
 @WebService
 public class EstoqueWS {
     private ItemDao dao = new ItemDao();
 
-    public List<Item> getItens() {
-        System.out.println("Chamando todosItens()");
-        return dao.todosItens();
+    /* Anotações abaixo são dos exercicios opcionais, elas possibilitam Embrulhar
+     * o request e response. No caso do response não precisariamos do objeto ListaItens
+     * @ResponseWrapper(localName="itens")
+     * @RequestWrapper(localName="listaItens")*/
+    @WebMethod(operationName="todosOsItens")
+    @WebResult(name="itens")
+    public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
+        System.out.println("Chamando getItens()");
+        List<Filtro> lista = filtros.getLista();
+        List<Item> itensResultado = dao.todosItens(lista);
+        return new ListaItens(itensResultado);
     }
+
 }
