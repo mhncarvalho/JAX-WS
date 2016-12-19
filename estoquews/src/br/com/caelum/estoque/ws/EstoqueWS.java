951b4107a5d6da3046adfeebc28317729d3a2260
Marcio Carvalho <mhn.carvalho@gmail.com>
2016-12-18 23:33:04 -0200

    Aula 1 - O primeiro serviço SOAP

---
+++ estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -0,0 +1,18 @@
+package br.com.caelum.estoque.ws;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import br.com.caelum.estoque.modelo.item.Item;
+import br.com.caelum.estoque.modelo.item.ItemDao;
+
+@WebService
+public class EstoqueWS {
+    private ItemDao dao = new ItemDao();
+
+    public List<Item> getItens() {
+        System.out.println("Chamando todosItens()");
+        return dao.todosItens();
+    }
+}
